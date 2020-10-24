fdb696b29d1fde1d2ace9bb174d617381527eeba
Abhinav Tiwari <58651105+abhinavIsRockstar@users.noreply.github.com>
2020-10-24 14:13:42 +0530

    created static folder
    I have created static folder and ejs template by which I manipulate the static html files and others

--- server.js
+++ server.js
@@ -1,19 +1,101 @@
 console.log('You are inside hole...PINK HOLE');
-
 const express = require('express');
 const app = express();
+const joi = require('joi');
 const path = require('path')
 const bodyParser = require('body-parser');
+// const sendMail = require('./mail');
+
+const nodeMailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
-app.set(bodyParser.json());
+// app.set(bodyParser.json());
+app.use(express.json())
 
-app.use('/',express.static(path.join(__dirname,'/')));
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.set('view engine', 'ejs')
+// console.log(__dirname,'dirname');
+
+
+const auth = {
+    auth : {
+        api_key : 'a9942c316ff9e44354d667563767f981-2fbe671d-23a44bc7',
+        domain : 'sandboxa6405aa2cf0f40c6903659c4cf28f99e.mailgun.org'
+    }
+}
+
+const transporter = nodeMailer.createTransport(mailGun(auth));
+let value ='jhkjh';
+
 
 app.get('/', (req,res)=>{
+    // let pathroute = (path.join(__dirname,'/static','/index.html'))
+    // console.log(pathroute,'pathroute')
+    console.log('inside /')
+    // res.sendFile(path.join(__dirname,'/static','/index.html'));
+    res.render('index.ejs',{data:value})
+    // res.sendFile(path.join(__dirname,'static','index.html'));
+    
+})
 
-    res.sendFile(path.join(__dirname,'/index.html'));
+app.get('/success', (req,res)=>{
+    res.render('success.ejs',{data: 'ok'})
+})
+
+app.post('/submit_details', (req,res) => {
+    console.log('inside posttt');
+    
+    const {name,email,phone_number,text} = req.body;
+    console.log('data: ',req.body);
+
+    // sendMail(name,email,phone_number,text, function(err,data){
+    //     console.log(data,'data');
+    //     if(err){
+    //         res.status(500).json({message:'internal error'});
+    //         console.log(err,'error')
+    //     } else{
+    //         // res.status({message: 'Email sent!!'})
+    //     //    alert(success);
+    //         res.render('success.ejs')
+    //         console.log('success line 41');
+    //     }
+    // })
+    // res.json({message: 'message recieved'})    
+        
+        const mailOptions = {
+            sender: name,
+            from:email,
+            to:'abhinavtiwari2303@gmail.com',
+            phone_number : phone_number,
+            text: text,
+            
+        }   
+    
+        transporter.sendMail(mailOptions, function(err,info){
+            if(err){
+                // cb(err,null);
+            } else {
+                // cb(null,data);
+                console.log('inside mail sendMail');
+               //  renderSuccess()
+                // res.render('success.ejs');  
+                // alert('success');     
+                // res.render('success.ejs', {message: 'email has been sent!'}) ;                                         
+                
+            }            
+           
+        })        
+        // res.render('index.ejs', {message: 'email has been sent!'}) ;  
+            
+        // res.render('index.ejs',{data:'sent'});
+
+        // function renderSuccess(){
+        //      console.log('inside render function');
+             
+        //  }   
+
 })
 
 
---
+++ static/js/index.js
@@ -0,0 +1,90 @@
+console.log('inside index.js');
+
+const burgerBtn = document.querySelector('.burger-container');
+const navContent = document.querySelector('.nav-content');
+const portfolioLists = document.querySelector('.portfolio-lists');
+const featured_widget = document.querySelectorAll('.featured-widget-item');
+const array_featured_widget = Array.from(featured_widget);
+//const list_array = Array.from(portfolioLists[0].children)
+console.log(portfolioLists,'portfoliolists');
+console.log(array_featured_widget,'array_featured_widget');
+
+portfolioLists.addEventListener('click', e =>{
+    console.log(e.target.innerText,'event')
+    let text = e.target.innerText
+    
+    // e.target.classList.add('active');
+    let elem = e;
+    changeActiveState(elem,text);
+    activeFeaturedContent(text);
+})
+
+function changeActiveState(elem,text) {
+
+    console.log(elem.target,'elem');
+    console.log('inside changeActive state');
+
+    let listItems = document.querySelectorAll('.portfolio-item');
+    let listArray = Array.from(listItems);
+    console.log(listArray,'listArray');
+   
+
+    for(let i = 0;i<listArray.length;i++){
+
+        console.log('inside changeActive state loop');
+
+        if(listArray[i].innerText != text){
+            console.log('inside if loop')
+            listArray[i].classList.remove('active');
+        }
+        elem.target.classList.add('active');
+        
+    }
+
+
+    // elem.classList.add('active');
+}
+
+function activeFeaturedContent(text){
+    console.log(text.toLowerCase(),'inside activeFeaturedContent');
+    console.log(array_featured_widget[0].dataset.type,'inside active loop')
+
+    for(let i=0;i<array_featured_widget;i++){
+        console.log(array_featured_widget[i].dataset.type,'inside active loop')
+        if(array_featured_widget[i].dataset.type !=text.toLowerCase()){
+            array_featured_widget[i].target.classList.add('disable_type')
+        }
+    }
+
+}
+
+
+// portfolioLists.addEventListener('click', (e)=>{
+//     console.log(e.target,'event');
+// })
+
+const screenWidth = window.innerWidth;
+let navBtnClicked = false;
+burgerBtn.addEventListener('click', (e)=>{
+    console.log(e,'e');
+    let nav = document.querySelector('.navigation');
+    if(!navBtnClicked){
+        nav.style.height = '275px';
+        // navContent.style.display = 'block';
+        // nav.style.transition = 'height .3s linear'
+        navBtnClicked = true;
+    } else {
+        nav.style.height = '55px';
+        navBtnClicked = false;
+        // navContent.style.display = 'none';
+    }    
+})
+
+console.log(screenWidth,' width');
+
+
+// if(screenWidth > 991){
+//     burgerBtn.style.display = 'none';
+// } else {
+//     burgerBtn.style.display = 'block';
+// }
